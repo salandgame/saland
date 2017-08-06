a7e7679895ba19aa789e6ff150c2dad4a8773161
sago007 <poul@poulsander.com>
2017-08-06 20:58:47 +0200

    Added Copyright notice. GPL for all the Saland specific code

--- src/saland.cpp
+++ src/saland.cpp
@@ -1,3 +1,26 @@
+/*
+===========================================================================
+ * Saland Adventures
+Copyright (C) 2014-2017 Poul Sander
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/
+
+Source information and contacts persons can be found at
+https://github.com/sago007/saland
+===========================================================================
+*/
+
 #include <iostream>
 #include <boost/program_options.hpp>
 #include <SDL_mixer.h>
