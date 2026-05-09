8545cbf7dbe5d5fa35c6014229d7b689b3beec0d
Poul Sander <poul@poulsander.com>
2026-05-09 22:26:12 +0200

    Add the tilesheet for dungeon

---
+++ data/maps/tiles_dungeon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_dungeon" tilewidth="32" tileheight="32" tilecount="21" columns="3">
+ <image source="../textures/tiles_dungeon.png" width="96" height="224"/>
+</tileset>
